6ca72793da455462b2569edf1b6a29eb1c00afbc
mayank_gwal <mayankgwal9@gmail.com>
2020-09-15 11:01:36 +0530

    src,jar added

---
+++ src/main/java/com/xyz/capstone_test/App.java
@@ -0,0 +1,30 @@
+package com.xyz.capstone_test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	static WebDriver driver;
+	static String webDriverPath="/home/ubuntu/chromedriver";
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.setProperty("webdriver.chrome.driver", webDriverPath );
+        ChromeOptions  options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:82/");
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+
+    }
+}
